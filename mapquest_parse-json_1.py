c250b97bf29cf93c869b131454f1ca9374d1a7b8
NetAcad DEVASC <changeme@example.com>
2021-10-11 07:06:00 +0000

    key included

--- mapquest_parse-json_1.py
+++ mapquest_parse-json_1.py
@@ -4,7 +4,7 @@ import requests
 main_api = "https://www.mapquestapi.com/directions/v2/route?"
 orig = "Quezon City, Philippines"
 dest = "Pasig City, Philippines"
-key = "your_api_key" #Replace with MapQuest Key
+key = "XqdCZb8GqAmkQEweVnsgNseyJZdgLcZu" #Replace with MapQuest Key
 
 url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest})
 
