dbbd031cc4aaacec358b705c814d9f51ad540f4b
unknown <angelanicole.castro.iics@ust.edu.ph>
2021-11-23 11:41:04 +0800

    automated testing

--- test_mapquest.py
+++ test_mapquest.py
@@ -3,13 +3,32 @@ import mapquest
 
 class TestMapQuest(unittest.TestCase):
 
-    def test_checkOrigin(self):
+    def test_checkValidOriginnDest(self):
         result = mapquest.configLocation('Washington, D.C.', 'Baltimore, Md')
-        self.assertEqual(result[0], 'Washington, D.C.')
+        self.assertTrue(len(result[9]))
 
-    def test_checkDestination(self):
-        result = mapquest.configLocation('Washington, D.C.', 'Baltimore, Md')
-        self.assertEqual(result[1], 'Baltimore, Md')
+    def test_checkMissingDest(self):
+        result = mapquest.configLocation('Washington, D.C.', '')
+        self.assertEqual(result[1], 'Missing an entry for one or both locations.')
+
+    def test_checkSameOriginnDest(self):
+        result = mapquest.configLocation('Washington, D.C.', 'Washington, D.C.')
+        self.assertTrue(len(result[9]))
+
+    def test_checkUnreachDest(self):
+        result = mapquest.configLocation('Manila, Philippines', 'Tokyo Japan')
+        self.assertEqual(result[1], 'Invalid user inputs for one or both locations.')
+
+    def test_checkQuit(self):
+        result = mapquest.configLocation('q', 'q')
+        self.assertIsNone(result)
+    # def test_checkDestination(self):
+    #     result = mapquest.configLocation('Washington, D.C.', 'Baltimore, Md')
+    #     self.assertEqual(result[2], 'Missing an entry for one or both locations.')
+
+    # def test_checkDestination(self):
+    #     result = mapquest.configLocation('Washington, D.C.', 'Baltimore, Md')
+    #     self.assertEqual(result[1], 'Baltimore, Md')
 
 if __name__ == '__main__':
     unittest.main()
